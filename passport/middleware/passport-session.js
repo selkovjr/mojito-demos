afeefe0c0764d25cd3c329d31445d8dba8ed8cde
Gene Selkov <selkovjr@gmail.com>
2013-05-06 14:42:27 +0100

    redirect unauthenticated routes to /login

--- passport/middleware/passport-session.js
+++ passport/middleware/passport-session.js
@@ -1,31 +1,29 @@
-var passport = require('passport');
-//module.exports = passport.session();
+/*global require: false, module: false, console: false */
 
-
-var passport_session = passport.session();
-
-/**
-module.exports = function (req, res, next) {
-    // we want to take control of the middleware so we can
-    // initialize and also attach passport into req object
-    req.passport.session();
-    
-    next();
-};
-**/
-
+var
+  passport = require('passport'),
+  passport_session = passport.session();
 
 module.exports = function (req, res, next) {
-    // we want to take control of the middleware so we can
-    // initialize and also attach passport into req object
-    passport_session(req, res, function (err) {
-        if (err) {
-            return next(err);
-        }
-        // Not sure if i attach to anything
-        console.log("passport session user " + req._passport.session.user);
+  console.log('passport-session middleware url: ' + req.url);
 
-        // returning control back to mojito flow
-        next();
-    });
+  // we want to take control of the middleware so we can
+  // initialize and also attach passport into req object
+  passport_session(req, res, function (err) {
+    if (err) {
+      return next(err);
+    }
+    // Not sure if i attach to anything
+    if (!req.isAuthenticated() && !req.url.match(/login/)) {
+      console.log('middleware/passport-session/passport session(): unauthenticated; redirecting');
+      // res.send('Please <a href="/login">log in</a>');
+      req.method = 'post';
+      res.redirect('/login');
+    }
+    else {
+      console.log('middleware/passport-session/passport session(): user ' + req._passport.session.user);
+      // returning control back to mojito flow
+      next();
+    }
+  });
 };
