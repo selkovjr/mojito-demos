7e506ff7b1b4ac37f5aa522c33ae2e1b41f20179
Gene Selkov <selkovjr@gmail.com>
2013-05-08 12:37:01 +0100

    jslint

--- passport/mojits/framed/controller.server.js
+++ passport/mojits/framed/controller.server.js
@@ -1,32 +1,23 @@
 YUI.add('framed', function(Y, NAME) {
   Y.namespace('mojito.controllers')[NAME] = {
-  	
-    index: function(ac) {
-      	
-			
-			req = ac.http.getRequest();
-			userInfo = req.user;
-			
-			
-			
-			var login_url = ac.url.make('login', 'index', '');
-			
-			
-			
-			if(userInfo){
-			     var username =userInfo.username;    
-			}
-			
 
-      		ac.done(
-      			{
-      				app_name:'Is there a user info object ' + Y.dump( userInfo),
-      				l_url: login_url,
-      				test: "testface",
-      				username: username
-      			}
-      			
-      			);
+    index: function(ac) {
+      req = ac.http.getRequest();
+      var
+        userInfo = req.user,
+        login_url = ac.url.make('login', 'index', ''),
+        username;
+
+      if(userInfo){
+        username = userInfo.givenName;
+      }
+
+      ac.done({
+        app_name:'Is there a user info object ' + Y.dump( userInfo),
+        l_url: login_url,
+        test: "testface",
+        username: username
+      });
     }
   };
 }, '0.0.1', {requires: ['mojito','mojito-intl-addon', 'mojito-url-addon', 'mojito-http-addon', 'dump']});
